f7a28fc06b3491032d379b83b41f218ad20cf6b8
Arpan KC <nipeshkc7@gmail.com>
2022-01-22 09:16:34 +1100

    add nodemon

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
+})
+
+app.get('/pictures', (req, res) => {
+    return res.json([
+        "https://res.cloudinary.com/fidohuncloud/image/upload/v1642794180/eyrfrf8orusaz2bnq7pj.jpg",
+        "https://res.cloudinary.com/fidohuncloud/image/upload/v1642795673/rrqsddzbjwtfkrm1vbco.jpg"
+    ]);
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
