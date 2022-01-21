35977d48a7bcdbc8f5fa2d6d5153263fae47e462
Arpan KC <nipeshkc7@gmail.com>
2022-01-22 10:44:48 +1100

    GET /pictures

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path');
+
+// app.use(cors({
+//     origin: 'http://localhost:8080'
+// }));
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
 })
 
 app.get('/pictures', (req, res) => {
